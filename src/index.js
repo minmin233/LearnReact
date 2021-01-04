bfd43c0a434205024a83e5c3240860fe5833e6d1
unknown <1@qunhe-incsh12.qunhe-inc.com>
2021-01-04 19:44:26 +0800

    父子组件通信案例

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './01_组件通信案例/App';
+import './01_组件通信案例/style.css';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
